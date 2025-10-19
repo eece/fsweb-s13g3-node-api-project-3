7ae17f02ac73339243c29bccd99f99175a2a775f
eece <eece90@gmail.com>
2025-10-19 21:46:31 +0300

    Add dotenv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // require your server and launch it
 const server = require('./api/server.js');
-
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
